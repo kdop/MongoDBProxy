da01aa7a4b645122cee9b8697eef3d100ef10330
Kostas <64cores@gmail.com>
2024-08-01 12:50:53 +0300

    PR32: Update pymongo3_durable_cursor.py

--- mongo_proxy/__init__.py
+++ mongo_proxy/__init__.py
@@ -1,10 +1,18 @@
 from .mongodb_proxy import MongoProxy
 from .durable_cursor import DurableCursor, MongoReconnectFailure
-from .pymongo3_durable_cursor import PyMongo3DurableCursor
+
+import pymongo
 
 __all__ = [
     'MongoProxy',
     'DurableCursor',
     'MongoReconnectFailure',
-    'PyMongo3DurableCursor',
 ]
+
+if pymongo.version_tuple[0] == 3:
+    from .pymongo3_durable_cursor import PyMongo3DurableCursor
+    __all__.append('PyMongo3DurableCursor')
+else:
+    from .pymongo4_durable_cursor import PyMongo4DurableCursor
+    __all__.append('PyMongo4DurableCursor')
+
